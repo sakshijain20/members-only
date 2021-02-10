b8e89e5f426e54d46cd9a1b7fe9078292fc9d706
Sakshi Madrecha <jain.sakshi1099@gmail.com>
2021-02-10 20:46:05 +0530

    added model and controller for posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_10_151015) do
+ActiveRecord::Schema.define(version: 2021_02_10_151405) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +33,5 @@ ActiveRecord::Schema.define(version: 2021_02_10_151015) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
