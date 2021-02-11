397062235090d92449360c2d08711f1624425251
Sakshi Madrecha <jain.sakshi1099@gmail.com>
2021-02-11 15:37:35 +0530

    added authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,60 @@
 class PostsController < ApplicationController
+	#before_action :authenticate ,only: [:new,:create]
+
+	
+    def index
+        @post = Post.all
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def edit
+    end
+    
+    def create
+	    @post = Post.new(post_params)
+
+	    respond_to do |format|
+	      if @post.save
+	        format.html { redirect_to @post, notice: "Post was successfully created." }
+	        format.json { render :show, status: :created, location: @post }
+	      else
+	        format.html { render :new, status: :unprocessable_entity }
+	        format.json { render json: @post.errors, status: :unprocessable_entity }
+	      end
+	    end
+  	end
+
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: "Post was successfully updated." }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
 end
+
+private
+
+  def post_params
+    params.require(:post).permit(:title, :body,:user_id)
+  end
